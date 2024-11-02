9f651d9d733f80c883d422bb8b0e18e71e2d46ec
Pedro Geovani <132714606+PedroGeovani@users.noreply.github.com>
2024-11-01 22:42:44 -0300

    (feature)[Frontend]primevue installation and configuration (#5)

--- Gerenciador_de_Ar-condicionado/src/App.vue
+++ Gerenciador_de_Ar-condicionado/src/App.vue
@@ -1,17 +1,11 @@
 <script lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 export default {
   name: 'app'
 }
 </script>
 
 <template>
-  <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink> | 
-        <RouterLink to="/register">Registro</RouterLink>
-      </nav>  
-  </header>
-
+  <Navbar />
   <RouterView />
 </template>
